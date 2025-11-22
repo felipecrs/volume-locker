220e8b8dd4d46a7d6fc8c9658ed00fd0aa37f450
Felipe Santos <felipecassiors@gmail.com>
2025-11-22 13:16:33 -0300

    Fix Cargo.toml for multiple OSes

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
-use winresource::WindowsResource;
-
 fn main() {
-    let mut res = WindowsResource::new();
-    // The first icon gets set as the executable icon
-    res.set_icon_with_id("icons/volume-locked.ico", "volume-locked-icon");
-    res.set_icon_with_id("icons/volume-unlocked.ico", "volume-unlocked-icon");
-    res.set_language(0x0009); // English
-    res.compile().unwrap();
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winresource::WindowsResource::new();
+        // The first icon gets set as the executable icon
+        res.set_icon_with_id("icons/volume-locked.ico", "volume-locked-icon");
+        res.set_icon_with_id("icons/volume-unlocked.ico", "volume-unlocked-icon");
+        res.set_language(0x0009); // English
+        res.compile().unwrap();
+    }
 }
