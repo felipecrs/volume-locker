f1052b922f678824c462f98ed9f699efaf8b0b67
Felipe Santos <felipecassiors@gmail.com>
2026-05-14 20:57:57 -0300

    refactor: use Result return in build.rs instead of panic
    Return Result<(), Box<dyn Error>> from main() and propagate errors
    with ? operator, eliminating the panic/expect entirely.

--- build.rs
+++ build.rs
@@ -1,12 +1,11 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(target_os = "windows")]
     {
         let mut res = winresource::WindowsResource::new();
-        // The first icon gets set as the executable icon
         res.set_icon_with_id("icons/volume-locked.ico", "volume-locked-icon");
         res.set_icon_with_id("icons/volume-unlocked.ico", "volume-unlocked-icon");
         res.set_language(0x0009); // English
-        res.compile()
-            .expect("Failed to compile Windows resources");
+        res.compile()?;
     }
+    Ok(())
 }
